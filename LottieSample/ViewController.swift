2f79175d51bce3bcc8a84b179b7d24c466f754d6
Hideki Matsuoka <matsuoka.hide@gmail.com>
2017-10-14 09:54:40 +0900

    Initial Commit

---
+++ LottieSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LottieSample
+//
+//  Created by matsuokah on 2017/10/14.
+//  Copyright © 2017 matsuokah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
